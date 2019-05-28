38053ed29f9c55a7c8267209a92eddfbd5bb09a0
miloszratajczyk <milosz2002ratajczyk@gmail.com>
2019-05-28 13:08:10 +0200

    Remove banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter 2048',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
